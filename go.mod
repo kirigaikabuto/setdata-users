d0aa9f1f9b9a086e2cdf11c8578988c6171a9055
Yerassyl <tleugazy98@gmail.com>
2021-07-28 22:13:08 +0600

    commits

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kirigaikabuto/setdata-users
 
 go 1.13
+
+require github.com/kirigaikabuto/setdata-common v1.0.0 // indirect
--- model.go
+++ model.go
@@ -1,1 +1,11 @@
 package setdata_users
+
+type User struct {
+	Id        string `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	LoginType string `json:"login_type"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
